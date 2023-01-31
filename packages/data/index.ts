32531fa754ef8adf71f9d2d0e06bc935ffa1f78a
Lachezar Tsochev <xtremescript@gmail.com>
2023-01-31 23:10:56 +0200

    slowly migrating to monorepo setup

---
+++ packages/data/index.ts
@@ -0,0 +1,4 @@
+export { default as ImageCategoriesData } from "./image_categories.json";
+export { default as ImageTagsData } from "./image_tags.json";
+export { default as ImageThumbnailData } from "./image_thumbnails.json";
+export { default as ImageData } from "./images.json";
