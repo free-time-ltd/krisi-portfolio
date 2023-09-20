4cff45a67ae5349d5ba81bf6e115a45bb4b5af3a
Lachezar Tsochev <xtremescript@gmail.com>
2023-09-20 14:16:50 +0300

    update nextjs to 13.5.1

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -8,11 +8,6 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  /* If trying out the experimental appDir, comment the i18n config out
-   * @see https://github.com/vercel/next.js/issues/41980 */
-  experimental: {
-    appDir: true
-  },
   images: {
     domains: [
       'localhost',
